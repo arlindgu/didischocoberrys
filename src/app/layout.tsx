91805756939e4bf15ec75638e696aa20ed4de6f5
arlindgurgurovci <155858954+arlindgurgurovci@users.noreply.github.com>
2025-07-19 13:47:46 +0200

    imported modalmenu rather than navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,26 +2,14 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import { Instrument_Serif, Instrument_Sans, Inter } from "next/font/google";
+import { Fredoka } from "next/font/google";
+import ModalMenu from "@/components/ModalMenu";
 
-const instrumentSerif = Instrument_Serif({
+const fredoka = Fredoka({
   subsets: ["latin"],
-  weight: ["400"],
-  variable: "--font-instrument-serif",
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-fredoka",
 });
-
-const instrumentSans = Instrument_Sans({
-  subsets: ["latin"],
-  weight: ["400"],
-  variable: "--font-instrument-sans",
-});
-
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
-  display: "swap",
-
-})
 
 export const metadata: Metadata = {
   title: "didischocoberrys",
@@ -35,9 +23,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${instrumentSerif.variable} ${instrumentSans.variable} ${inter.variable} subpixel-antialiased`}>
+      <body className={` ${fredoka.variable} subpixel-antialiased`}>
         <main className="h-screen flex flex-col justify-between items-center">
           <Navbar />
+            <ModalMenu />
           <div className="p-4 flex flex-col mt-8 lg:max-w-5xl md:max-w-xl">
             {children}
           </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,35 +5,10 @@ import { SpinningText } from '../../components/motion-primitives/spinning-text';
 
 export default function Navbar() {
 
-    const HeaderLeft = {
-        about: { href: "/about", label: "Über Uns" },
-        instagram: { href: "https://www.instagram.com/didischocoberrys/", label: "Instagram" },
-
-
-    }
-
-    const HeaderUnder = {
-        pictures: { href: "/pictures", label: "Bilder" },
-
-    }
-
-    const HeaderRight = {
-        pricing: { href: "/pricing", label: "Preise" },
-        order: { href: "/order", label: "Bestellen" },
-
-    }
-
 
 
     return (
-        <header className="flex flex-row justify-between items-center gap-2 p-2">
-            <nav className='flex flex-col items-center gap-2'>
-                {Object.entries(HeaderLeft).map(([key, value]: [string, { href: string; label: string; }]) => (
-                    <Link href={value.href} key={key} className=''>{value.label}</Link>
-                ))}
-            </nav>
-            <div>
-                <div className='flex flex-col items-center gap-2'>
+        <header className="flex justify-between items-center gap-2 p-2 mt-8">
                     <Link href="/" className='text-chocolate font-bold hover:text-chocolate-300'>
                         <SpinningText
                             radius={3.5}
@@ -44,16 +19,6 @@ export default function Navbar() {
                             {`didis ♥ choco ♥ berrys ♥ `}
                         </SpinningText>
                     </Link>
-                    <Link href={HeaderUnder.pictures.href}>
-                        {HeaderUnder.pictures.label}
-                    </Link>
-                </div>
-            </div>
-            <nav className="flex flex-col gap-2">
-                {Object.entries(HeaderRight).map(([key, value]: [string, { href: string; label: string; }]) => (
-                    <Link href={value.href} key={key} className=''>{value.label}</Link>
-                ))}
-            </nav>
         </header>
     );
 }
